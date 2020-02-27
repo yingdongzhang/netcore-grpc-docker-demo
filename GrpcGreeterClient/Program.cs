3a3de5ee353b6d7290480b2dd16b537541a2a29e
Yingdong Zhang <zhangyingdong89@gmail.com>
2020-02-27 23:33:41 +1100

    inital commit

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using GrpcGreeter;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://grpc_greeter:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" }
+            );
+
+            Console.WriteLine("Greeting: " + reply.Message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # netcore-grpc-docker-demo
 Playing with netcore + gRPC + docker
+
+### Build Server
+```bash
+cd ./GrpcGreeter
+docker build . -t dz/grpc_greeter:1
+```
+### Start Server
+```bash
+docker run -p 5000:5000 --name grpc_greeter  dz/grpc_greeter:1
+```
+### Build Client
+```bash
+cd ./GrpcGreeterClient
+```
+### Run Client
+```bash
+docker run --link grpc_greeter dz/grpc_greeter_client:1
+```
